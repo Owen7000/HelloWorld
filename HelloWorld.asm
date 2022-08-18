1b91b02dc550d9edb4bcfdff9d5396e05c431a94
Owen From Lifelessstudio <77976026+Owen7000@users.noreply.github.com>
2022-08-18 15:12:00 +0100

    Create HelloWorld.asm

---
+++ HelloWorld.asm
@@ -0,0 +1,29 @@
+;
+; A simple Hello World Program written in Assembly Language
+;
+
+
+int 0x19
+mov abx, 'H'
+int 0x19
+mov abx, 'e'
+int 0x19
+mov abx, 'l'
+int 0x19
+mov abx, 'l'
+int 0x19
+mov abx, 'o'
+int 0x19
+mov abx, ''
+int 0x19
+mov abx, 'W'
+int 0x19
+mov abx, 'o'
+int 0x19
+mov abx, 'r'
+int 0x19
+mov abx, 'l'
+int 0x19
+mov abx, 'd'
+int 0x19
+mov abx, '!'
